6c0f1d91bb61496e7b0839c4a507fc5e82307bf2
oz izhak <oz@web-pick.com>
2019-03-14 18:23:38 +0200

    start app

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
-import { UserDetalisComponent } from './user-detalis/user-detalis.component';
+import { HeaderComponent } from "./header/header.component";
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserDetalisComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
   ],
   imports: [
     BrowserModule,
