d0f694260cd6921bf982c11719dd0ce3060dfbec
oz izhak <ozizhak5540@gmail.com>
2019-03-15 19:31:18 +0200

    adding navbar and recipe list. acreate Recipe model and export it to recipe-list.

--- my-first-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-first-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipe: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply Test', 'https://images.media-allrecipes.com/images/56589.png')
+  ];
 
   constructor() { }
 
