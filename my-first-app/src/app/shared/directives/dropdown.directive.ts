a1a833ed6f9fe2265ada7585e91a8f004e3daa5f
oz izhak <ozizhak5540@gmail.com>
2019-03-20 01:40:08 +0200

    start a Services injectors

--- my-first-app/src/app/shared/directives/dropdown.directive.ts
+++ my-first-app/src/app/shared/directives/dropdown.directive.ts
@@ -1,11 +1,16 @@
-import {Directive, HostListener} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  @HostListener()
+  @HostBinding('class.open') isOpen:boolean = false;
+  constructor() {}
 
-  constructor() { }
+//  this.renderEl.addClass(this.elRef.nativeElement, 'open')
+  
+  @HostListener('click', ['$event.target']) onClick(event: Event) {
+    this.isOpen = !this.isOpen;
+  }
 
 }
